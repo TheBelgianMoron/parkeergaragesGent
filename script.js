d1648f8581c076686318fc886e3c3a8f721f030e
Vermeulen Ignace <ignace.vermeulen@leerling.gent.be>
2025-01-24 13:49:18 +0100

    Edited index.html, added Leaflet map, edited main.css and script.js

--- script.js
+++ script.js
@@ -2,6 +2,14 @@
 const apiURL = "https://data.stad.gent/api/explore/v2.1/catalog/datasets/bezetting-parkeergarages-real-time/records?limit=20";
 const parkingContainer = document.getElementById('parkingData');
 
+// Add Leaflet map to the page
+let map = L.map('map').setView([51.053581, 3.722969], 13);
+// Add base map
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
 // Function to fetch and display parkingdata
 async function fetchDataAndDisplayData() {
     try {
@@ -17,7 +25,7 @@ async function fetchDataAndDisplayData() {
 
         //display data
             parkingData.forEach(parkingData => {
-                const { name, occupation, availablecapacity, isopennow } = parkingData;
+                const { name, occupation, availablecapacity, isopennow, location } = parkingData;
                 const occupied = occupation;
                 const status = isopennow ? 'open' : 'closed';
 
@@ -31,12 +39,25 @@ async function fetchDataAndDisplayData() {
                     <p class="${isopennow ? 'open' : 'closed'}">Status: ${status}</p>
                 `;
                 parkingContainer.appendChild(parkingElement);
+                //add marker to map
+                displayOnMap(location)
             });
     } catch (error) {
         console.error('Fault in fetchDataAndDisplayData:', error);
     } finally {
         console.log('Function fetchDataAndDisplayData completed');
     };
+};
+
+function displayOnMap(location) {
+    const { lat, lon } = location;
+    const customIcon = L.icon({
+        iconUrl: 'https://cdn-icons-png.flaticon.com/512/252/252025.png',
+        iconSize: [32, 32],
+        iconAnchor: [16, 32],
+        popupAnchor: [0, 32]
+    });
+    L.marker([lat, lon], { icon: customIcon }).addTo(map);
 };
 
 fetchDataAndDisplayData();
